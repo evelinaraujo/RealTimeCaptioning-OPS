069974d3cbc859e5f8030fcca2563a5f19f79fef
evelyn araujo <evelynaraujo@evelyns-MacBook-Pro.local>
2017-11-13 22:24:14 -0800

    Changes

--- packer/rtc-provision.sh
+++ packer/rtc-provision.sh
@@ -30,14 +30,6 @@ sudo apt-get install -y build-essential
 echo "Check node version"
 nodejs --version
 
-echo "Install mongodb"
-
-echo "Import the public key used by the package management system "
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-
-echo "Create a list file for mongodb"
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
-
 echo "Reload local package database"
 sudo apt-get update
 
@@ -63,12 +55,16 @@ npm install -g @angular/cli
 echo "Run npm install"
 npm install
 
-### Left off right here when running on 172.16.1.111 #######
-
 echo "Run npm install inside the backend directory"
 cd backend
 npm install
 node indexjs
+
+/*
+echo "Install mongodb"
+
+echo "Import the public key used by the package management system "
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 
 echo "Install version of mongodb"
 sudo apt-get install -y mongodb-org
@@ -82,10 +78,9 @@ sudo mkdir db
 sudo chown -R `id -un` /data/db
 cd ~
 
-
 echo "Start mongodb"
 cd /data && sudo mongod
-
+*/
 
 #  environment variables for the project
 # database-example
